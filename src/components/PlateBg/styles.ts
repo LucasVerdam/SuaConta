bde2f2102581befb37dce0695b0f464fc6d57ff1
LucasVerdam <lucanis12t@gmail.com>
2023-06-05 18:18:35 -0300

    Banco de Dados com PostgreSql para histórico do app.

--- src/components/PlateBg/styles.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-import { themes } from '../../global/styles/themes';
-
-export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#C4C4C4'
-    }
-});
